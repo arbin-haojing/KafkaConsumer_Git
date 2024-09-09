05b0f2c286e9028cc29bbc89601422096cdf7d71
haojing lin <haojing_lin@arbin.com>
2024-09-09 09:54:14 +0800

    update

--- KafkaConsumer/KafkaConsumer/Program.cs
+++ KafkaConsumer/KafkaConsumer/Program.cs
@@ -15,14 +15,6 @@ namespace KafkaConsumer
         [STAThread]
         static void Main()
         {
-            double Tp = 16988325647717000;
-            double TA1 = 16988325621778000;
-            double Ta2 = 16988883016601000;
-            double Vp;
-            double Va1 = 59.188;
-            double Va2 = 25.24172;
-
-            Vp = (Tp - TA1) / (Ta2 - TA1) * (Va2 - Va1) + Va1;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
